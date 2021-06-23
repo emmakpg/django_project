40daac1ef01946a9e0a9af8441ccc05784a814d0
EKAY <emmakpg@yahoo.com>
2021-06-23 03:02:29 +0300

    update

--- dj_blog/blog/views.py
+++ dj_blog/blog/views.py
@@ -1,30 +1,11 @@
 from django.shortcuts import render
+from .models import Post
  
-post = [
-     {
-         'author':'Emmanuel Kpogo',
-         'title':'Why we got to live',
-         'content':'First post content',
-         'Date':'24th June 2004'
-     },
-     {
-         'author':'Joshua Kpogo',
-         'title':'We are been perfected',
-         'content':'Second post content',
-         'Date':'27th April 2004'
-     },
-     {
-         'author':'Stephen Kpogo',
-         'title':'Great things take time',
-         'content':'Third post content',
-         'Date':'24th March 2014'
-     }
- ]
 
 
 def home(request):
     context = {
-    'posts': post
+    'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
@@ -32,4 +13,3 @@ def home(request):
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
 
-# Create your views here.
